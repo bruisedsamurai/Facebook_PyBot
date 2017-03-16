fa162560568282cc03305cef6be50d8144fad9ac
hundredeir <hundredeir@protonmail.com>
2017-03-16 22:20:28 +0530

    Added one more example
    This example send a get request to facebook to get user info and send
    back user info in form of a template to user.
    
    Also, updated manifest

---
+++ examples/userInfo.py
@@ -0,0 +1,26 @@
+"""
+Gets the user info and sendback to the user in the form of template form on being asked
+This program not teaches the use of getting user info but also how to send a generic template message
+"""
+import os
+
+import Facebook
+
+def main(message):
+	id=message.USER_ID
+	sent=send(PAGE_ACCESS_TOKEN)
+	if message.Message_Received.text is not None:
+		mess=message.Message_Received.text
+		if mess == "get me":
+			"""
+			get_UserInfo returns the first name,last name,url of the picture,their locale,timezone and gender of person whom id is passed as an arguement
+			"""
+        	fname, lname, picurl, locale, tz, gender = sent.get_UserInfo(id)
+        	ele = generate.element(generate, fname + " " + lname, gender + '\n' + locale, picurl)
+        	sent.sendGeneric_Template(id, ele)
+
+
+if __name__ == "__main__":
+    PORT = int(os.environ.get('PORT', '5000'))
+    webhook.startServer(main, host="0.0.0.0", port=PORT)  #start the webhook with main function passes as an arguement
+        
